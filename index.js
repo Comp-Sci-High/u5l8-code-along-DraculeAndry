828050395c7d7670906b37d58bef14505813fac7
Andry <andry.hernandez26@compscihigh.org>
2026-03-31 19:18:03 +0000

    I finished

--- index.js
+++ index.js
@@ -27,9 +27,21 @@ const Country = mongoose.model("Country", countrySchema, "Countries");
 // Create a POST route for "/add/country" that adds a country using the request body (3 points)
 // Use postman to add at least THREE different countries
 
+app.post('add/country', async (req, res) => {
+  const newCountry = await new Country({
+    country: req.body.country,
+    flagURL: req.body.flagURL,
+    population: req.body.population,
+    officialLanguage: req.body.officialLanguage,
+    hasNuclearWeapons: req.body.hasNuclearWeapons
+  }).save();
+})
 
 // Create a GET route for "/" that renders countries.ejs with every country from the Countries collection (1 point)
-
+app.get("/", async (req, res) => {
+  const countries = await Country.find({})
+  res.render('countries.ejs', { countries })
+})
 
 // Go to countries.ejs and follow the tasks there (2 points)
 
@@ -37,15 +49,26 @@ const Country = mongoose.model("Country", countrySchema, "Countries");
 // Create a dynamic PATCH route handler for "/update/{name}" that modifies the population of the country specified in the path (3 points)
 // Test this route on post man
 
-
+app.patch('/update/:name', async (req, res) => {
+  const updatedCountry = await Country.findOneAndUpdate(
+    { country: req.params.name },
+    { population: req.body.population },
+    { new: true }
+  );
+  res.json(updatedCountry)
+})
 
 // Create a DELETE route handler for "/delete/country" that deletes a country of your choice (3 points)
 // Test this route on post man
 
+app.delete("/delete/:country", async (req, res) => {
+  const deletedCountey = await Country.findOneAndDelete({country: req.params.name})
+  res.json(deletedCountry)
+})
 
 async function startServer() {
-  
-    // add your SRV string with a database called countries
+
+  // add your SRV string with a database called countries
   await mongoose.connect("mongodb+srv://SE12:CSH2026@cluster0.ytvmkmf.mongodb.net/?appName=Cluster0");
 
   app.listen(3000, () => {
